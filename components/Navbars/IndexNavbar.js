7f3ee673d7ca7825aa4c133bad2833aff91b1dda
behnam Aboutalebi <contact@behnam.io>
2020-12-01 13:12:41 +0330

    fix navbar telegram and logo

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -11,20 +11,19 @@ export default function Navbar(props) {
             <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black shadow">
                 <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                     <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        <Link href="/">
+                        <Link href="https://www.gohfm.com/sv/fa/">
                             <a
                                 className="text-red-500 text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
                                 href="#pablo"
                             >
                                 <img
-                                    src={require('assets/img/hfmarkets_logo.png')}
+                                    src={require('assets/img/hotforex_logo.png')}
                                 ></img>
                             </a>
                         </Link>
                         <button
                             className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                             type="button"
-                            onClick={() => {}}
                         >
                             <i className="fas fa-bars"></i>
                         </button>
@@ -39,20 +38,28 @@ export default function Navbar(props) {
                         }
                         id="example-navbar-warning"
                     >
-                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-                            <button
-                                className="bg-green-400 text-white active:bg-green-500 uppercase text-base px-12 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                type="button"
+                        <ul className="flex flex-col list-none lg:ml-auto">
+                            <a
+                                href="https://t.me/hotforexFinance"
+                                target="_blank"
                             >
-                                <i className="fas fa-credit-card"></i> Sales
-                                Support
-                            </button>
-                            <button
-                                className="bg-green-400 text-white active:bg-green-500 uppercase text-base px-12 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                type="button"
-                            >
-                                <i className="fas fa-wrench"></i> Tech Support
-                            </button>
+                                <button
+                                    className="bg-black text-white active:bg-green-500 uppercase text-base px-12 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                >
+                                    <span>ارتباط با بخش مالی هات فارکس</span>
+                                    <i className="fas fa-paper-plane ml-2 bg-blue-600 rounded-full h-8 w-8 py-2 pr-3"></i>
+                                </button>
+                            </a>
+                            <a href="https://t.me/hotforexiran" target="_blank">
+                                <button
+                                    className="bg-black text-white active:bg-green-500 uppercase text-base px-12 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                >
+                                    <span>ارتباط با بخش فنی هات فارکس</span>
+                                    <i className="fas fa-paper-plane ml-2 bg-blue-600 rounded-full h-8 w-8 py-2 pr-3"></i>
+                                </button>
+                            </a>
                         </ul>
                     </div>
                 </div>
